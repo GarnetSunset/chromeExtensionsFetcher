093a4c044eeb91c2e1bb22a752e6ad1c36e20086
James <p1n3tr335@gmail.com>
2018-03-14 17:50:23 -0400

    Zippity Doo Dah

--- folderGrabber.py
+++ folderGrabber.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 from six.moves.urllib.request import urlretrieve
 import ctypes, os, re, socket, subprocess, sys, time, zipfile
 
-userfolder = os.getenv('USERPROFILE')
-dldir = userfolder+"\Downloads"
+userfolder = os.getenv('USERNAME')
+dldir = "C:\Users\\" + userfolder +"\Downloads"
 pfdir = "C:\Program Files"
 pfdir86 = "C:\Program Files (x86)"
 names = ""
@@ -21,10 +21,10 @@ def admin():
 
 choice = raw_input("1. Your machine or 2. Someone else's?\n>")
 
-def makedirlocal(bazinga, choice, dldir, hostnameIP):
+def makedirlocal(bazinga, choice, dldir, pfdir, pfdir86, hostnameIP):
     global directory_list
+    directory_list = list()
     if choice == "1":
-        directory_list = list()
         for x in os.listdir(bazinga):
             path = os.path.join(bazinga, x)
             if os.path.isdir(path):
@@ -32,25 +32,33 @@ def makedirlocal(bazinga, choice, dldir, hostnameIP):
             if os.path.isfile(path):
                 directory_list.append(x)
 
-if choice == "2":
-    hostnameIP = raw_input("Input hostname or IP\n>")
-    username = raw_input("Input username\n>")
-    if admin():
-        batcmd = "dir \"\\\\"+hostnameIP+"\c$\Users\\"+username+"\Downloads\""
-        result = subprocess.check_output(batcmd, shell=True)
-        directory_list = list()
-        while("<DIR>" in result):
-            dirLoc = result.find("\n")
-            result = result[dirLoc+40:]
-            newLine = result.find("\n")
-            name = result[:newLine]
-            name.rstrip()
-            name = name[:-1]
-            if len(name) > 2:
-                directory_list.append(name)
-            
-    else:
-        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+    if choice == "2":
+        hostnameIP = raw_input("Input hostname or IP\n>")
+        username = raw_input("Input username\n>")
+        if admin():
+            if bazinga == dldir:
+                batcmd = "dir \"\\\\" + hostnameIP + "\c$\Users\\" + username + "\Downloads\""
+            if bazinga == pfdir:
+                batcmd = "dir \"\\\\" + hostnameIP + "\c$\Program Files\""
+            if bazinga == pfdir86:
+                batcmd = "dir \"\\\\" + hostnameIP + "\c$\Program Files (x86)\""
+            result = subprocess.check_output(batcmd, shell=True)
+            print(result)
+            directory_list = list()
+            while("PM" or "AM" in result):
+                dirLoc = result.find("\n")
+                result = result[dirLoc+40:]
+                newLine = result.find("\n")
+                name = result[:newLine]
+                name.rstrip()
+                name = name[:-1]
+                if len(name) > 2:
+                    directory_list.append(name)
+                
+        else:
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+
+
 
 open("Machines/" + hostnameIP + "-dirsnfiles.txt", 'w').close()
 
@@ -58,7 +66,7 @@ text_file = open("Machines/" + hostnameIP + "-dirsnfiles.txt", "a")
 
 text_file.write("------------Downloads------------\n")
 
-makedirlocal(bazinga=dldir, choice=choice, dldir=dldir, hostnameIP=hostnameIP)
+makedirlocal(bazinga=dldir, choice=choice, dldir=dldir, pfdir=pfdir, pfdir86=pfdir86, hostnameIP=hostnameIP)
 directory_list.sort()
 for x in directory_list:
     names = names + x
@@ -67,7 +75,7 @@ for x in directory_list:
 
 text_file.write("------------ProgramFiles------------\n")
 
-makedirlocal(bazinga=pfdir, choice=choice, dldir=dldir, hostnameIP=hostnameIP)
+makedirlocal(bazinga=pfdir, choice=choice, dldir=dldir, pfdir=pfdir, pfdir86=pfdir86, hostnameIP=hostnameIP)
 directory_list.sort()
 for x in directory_list:
     names = names + x
@@ -76,7 +84,7 @@ for x in directory_list:
     
 text_file.write("------------ProgramFilesx86------------\n")
 
-makedirlocal(bazinga=pfdir86, choice=choice, dldir=dldir, hostnameIP=hostnameIP)
+makedirlocal(bazinga=pfdir86, choice=choice, dldir=dldir, pfdir=pfdir, pfdir86=pfdir86, hostnameIP=hostnameIP)
 directory_list.sort()
 for x in directory_list:
     names = names + x
